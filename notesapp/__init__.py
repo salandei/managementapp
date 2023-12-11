a1a55ee322d0374c9a7ba6de94b156eee43c4f47
Solomon Alan-Dei <alandei.solomon@gmail.com>
2023-12-11 11:48:03 +0000

    fix db error

--- notesapp/__init__.py
+++ notesapp/__init__.py
@@ -3,33 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from os import path
 
-db = SQLAlchemy()
 DB = 'database.db'
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'osff-notes-app'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ path.join(app.instance_path, DB)
-    db.init_app(app)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'osff-notes-app'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ path.join(app.instance_path, DB)
+db = SQLAlchemy(app)
 
-    from .views import views
-    from .auth import auth
+from .views import views
+from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+app.register_blueprint(views, url_prefix='/')
+app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
-    create_db(app)
+from .models import User, Note
+create_db(app)
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-    return app
+return app
 
 def create_db(app):
     if not path.exists(path.join(app.instance_path, DB)):
